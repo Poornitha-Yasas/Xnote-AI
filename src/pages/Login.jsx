db1d8786f5c147362ab5b1f24c0eddef142ca83a
Poornitha-Yasas <poornithayasasx0@gmail.com>
2025-02-11 20:49:33 +0530

    Update UI

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -131,7 +131,7 @@ const Login = () => {
           />
 
           <p className='absolute bottom-10 left-12 right-12 z-10 text-displayLarge font-semibold leading-tight text-right text-light-onSurface drop-shadow-sm 2xl:text-[72px]'>
-            Chat with Xnote to supercharge your ideas.
+            Spark your ideas with Xnote.
           </p>
         </div>
       </div>
