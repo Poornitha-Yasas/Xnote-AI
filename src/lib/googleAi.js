1bd09de01077f5f7020a70a8ee7242772162c455
Poornitha-Yasas <poornithayasasx0@gmail.com>
2025-02-11 16:46:23 +0530

    modle update

--- src/lib/googleAi.js
+++ src/lib/googleAi.js
@@ -10,6 +10,6 @@ import { GoogleGenerativeAI } from '@google/generative-ai';
 
 const genAI = new GoogleGenerativeAI(import.meta.env.VITE_GEMINI_API_KEY);
 
-const model = genAI.getGenerativeModel({ model: 'gemini-1.5-flash' });
+const model = genAI.getGenerativeModel({ model: 'gemini-2.0-flash' });
 
 export default model;
